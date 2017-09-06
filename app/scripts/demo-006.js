b4ffdc58d5bd720404f4a63d786dea0c07db5c04
aicekiller <1017634520@qq.com>
2017-09-06 16:00:21 +0800

    modify

--- app/scripts/demo-006.js
+++ app/scripts/demo-006.js
@@ -1,12 +1,36 @@
-window.onload = function () {
+//http://img0.puzeyf.com/cmsres/20170720/ac247ce8-bfa9-47d7-ba28-a0a52888828f.png
+
+var fs = require('fs');
+
+function readLines(input, func) {
+  var remaining = '';
+
+  input.on('data', function(data) {
+    remaining += data;
+    var index = remaining.indexOf('\n');
+    var last  = 0;
+    while (index > -1) {
+      var line = remaining.substring(last, index);
+      last = index + 1;
+      func(line);
+      index = remaining.indexOf('\n', last);
+    }
+
+    remaining = remaining.substring(last);
+  });
+
+  input.on('end', function() {
+    if (remaining.length > 0) {
+      func(remaining);
+    }
+  });
+}
+
+function func(data) {
+  console.log('Line: ' + data);
+}
+
+var input = fs.createReadStream('../images/slide_1.jpg');
+readLines(input, func);
 
 
-};
-
-var sliderUI = document.getElementById('slider-img-ul');
-var sliderNum = document.getElementById('slider-num');
-
-
-setInterval(function () {
-
-}, 1000);
